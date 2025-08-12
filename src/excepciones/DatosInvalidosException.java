3801d5c032f595133f0b0ce46d71c22396e67a00
qadri <qadri@Wiston>
2025-08-12 13:40:34 -0600

    Tercer Commits: Se creo el paquete excepciones con la clase denominada DatosInvalidosException

---
+++ src/excepciones/DatosInvalidosException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author qadri
+ */
+public class DatosInvalidosException extends Exception {
+    public DatosInvalidosException(String mensaje) {
+        super(mensaje);
+    }
+}
